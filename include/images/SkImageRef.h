99d118f65a9d99c610396ea78b27faadb8d0b9cd
Romain Guy <romainguy@google.com>
2010-08-19 14:46:14 -0700

    Add an API to correctly copy the isOpaque flage of an ImageRef to Bitmap.
    Change-Id: I4aef8381bb6f722c0630ddc15a32a0ef07041613

--- include/images/SkImageRef.h
+++ include/images/SkImageRef.h
@@ -57,6 +57,12 @@ public:
     */
     bool getInfo(SkBitmap* bm);
     
+    /** Return true if the image can be decoded and is opaque. Calling this
+        method will decode and set the pixels in the specified bitmap and
+        set the opaque flag.
+     */
+    bool isOpaque(SkBitmap* bm);
+    
     SkImageDecoderFactory* getDecoderFactory() const { return fFactory; }
     // returns the factory parameter
     SkImageDecoderFactory* setDecoderFactory(SkImageDecoderFactory*);
