f0f7710af3155c8bbd566c32249e72a968459c78
Derek Sollenberger <djsollen@google.com>
2013-06-27 13:49:42 -0400

    Remove legacy_skia library from the build.
    We no longer support saved page playback from the original
    android browser so this library is not required.
    
    Change-Id: I51d68366512372b5196098b2d5d7dde5ee7522db

--- legacy/src/images/SkBitmapRegionDecoder.cpp
+++
@@ -1,7 +0,0 @@
-#include "SkBitmapRegionDecoder.h"
-
-bool SkBitmapRegionDecoder::decodeRegion(SkBitmap* bitmap, SkIRect rect,
-        SkBitmap::Config pref, int sampleSize) {
-    fDecoder->setSampleSize(sampleSize);
-    return fDecoder->decodeRegion(bitmap, rect, pref);
-}
