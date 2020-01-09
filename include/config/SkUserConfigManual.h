3eeeb18e73d19aa517df065dff21591b96c1187c
Mike Reed <reed@google.com>
2020-01-09 15:23:48 -0500

    add flag to stage new virtuals in SkCanvas
    See https://skia-review.googlesource.com/c/skia/+/263349
    
    Test: make
    
    Change-Id: If0b8f49c9fc1542f5115f7a3522bd6bed0278a58

--- include/config/SkUserConfigManual.h
+++ include/config/SkUserConfigManual.h
@@ -21,6 +21,7 @@
   // Legacy flags
   #define SK_IGNORE_GPU_DITHER
   #define SK_SUPPORT_DEPRECATED_CLIPOPS
+  #define SK_SUPPORT_LEGACY_CANVAS_MATRIX_VIRTUALS
  
   // Needed until we fix https://bug.skia.org/2440
   #define SK_SUPPORT_LEGACY_CLIPTOLAYERFLAG
