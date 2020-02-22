478232369924d0dd6bb87b964072639909895ecd
Mike Reed <reed@google.com>
2020-02-22 10:27:16 -0500

    switch to public SkM44
    Test: make
    
    Change-Id: I2470a276d6a1e3ed76ecb8b675349725cf82b12c

--- include/config/SkUserConfigManual.h
+++ include/config/SkUserConfigManual.h
@@ -24,7 +24,6 @@
   
   // Staging flags
   #define SK_LEGACY_PATH_ARCTO_ENDPOINT
-  #define SK_SUPPORT_EXPERIMENTAL_CANVAS44
  
   // Needed until we fix https://bug.skia.org/2440
   #define SK_SUPPORT_LEGACY_CLIPTOLAYERFLAG
