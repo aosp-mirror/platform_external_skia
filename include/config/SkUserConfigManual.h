fcf248b46cbfb795de5c59030ca8f180dcf72f15
Mike Reed <reed@google.com>
2020-03-03 16:57:21 -0500

    opt into new virtual for drawVertices
    Test: make
    
    Change-Id: I49135c2e678eaf9605ca8b10ef909d441e48d180

--- include/config/SkUserConfigManual.h
+++ include/config/SkUserConfigManual.h
@@ -24,7 +24,6 @@
   
   // Staging flags
   #define SK_LEGACY_PATH_ARCTO_ENDPOINT
-  #define SK_SUPPORT_LEGACY_DRAWVERTS_VIRTUAL
  
   // Needed until we fix https://bug.skia.org/2440
   #define SK_SUPPORT_LEGACY_CLIPTOLAYERFLAG
