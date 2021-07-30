041a67a595d4afb732a1b5cc97843b0b05d1cff0
Mike Reed <reed@google.com>
2021-07-30 10:29:27 -0400

    Add flag to stage API change
    Test: make
    
    Bug: 178700363
    Change-Id: I15d678d3896efb896d1b87f11f32df7787a53577

--- include/config/SkUserConfigManual.h
+++ include/config/SkUserConfigManual.h
@@ -23,6 +23,7 @@
 
   // Staging flags
   #define SK_LEGACY_PATH_ARCTO_ENDPOINT
+  #define SK_SUPPORT_STROKEANDFILL
 
   // Needed until we fix https://bug.skia.org/2440
   #define SK_SUPPORT_LEGACY_CLIPTOLAYERFLAG
